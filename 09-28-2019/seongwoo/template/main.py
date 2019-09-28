5c5069781f8740e3678b4c1ef46e84c5c74c57db
SeongwooPark <seongwoo@mymusictaste.com>
2019-09-28 15:16:45 +0900

    add algospot template

---
+++ 09-28-2019/seongwoo/template/main.py
@@ -0,0 +1,32 @@
+import os
+
+
+def solution():
+    return -1
+
+
+class ReadContext(object):
+    def __init__(self, file_name=None):
+        if not file_name:
+            file_name = 'input.txt'
+        dir_name = os.path.dirname(os.path.abspath(__file__))
+        if os.path.exists(f'{dir_name}/{file_name}'):
+            self._file_obj = open(f'{dir_name}/{file_name}', 'r')
+        else:
+            self._file_obj = None
+
+    def __enter__(self):
+        return self
+
+    def input(self):
+        return self._file_obj.readline() if self._file_obj else input()
+
+    def __exit__(self, _type, _value, _trace_back):
+        if self._file_obj:
+            self._file_obj.close()
+
+
+if __name__ == '__main__':
+    with ReadContext() as r:
+        print(r.input())
+        answer = solution()
